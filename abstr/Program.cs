edaef7e4c195bd4279e3245e3e344ef322cf5662
bondarevVV <90005849+bondarevVV@users.noreply.github.com>
2022-02-05 21:16:01 +0300

    First commit

---
+++ abstr/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstr
+{
+
+    abstract class Person
+    {
+        public abstract void Age();
+
+        public virtual void Name() 
+        {
+        }
+    }
+
+    class Student : Person
+    {
+        public override void Age()
+        {
+            Console.WriteLine("18");
+        }
+
+        public override void Name()
+        {
+            base.Name();
+        }
+    }
+
+    class Teacher : Person
+    {
+        public override void Age()
+        {
+            Console.WriteLine("25+ Лет");
+        }
+
+        public void Lesson()
+        {
+            Console.WriteLine("Предмет - ");
+        }
+    }
+
+    class ZavKaf : Person
+    {
+        public override void Age()
+        {
+            Console.WriteLine("30+ Лет");
+        }
+
+        public void Kafedra()
+        {
+            Console.WriteLine("Кафедра - ");
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher();
+            Student student = new Student();
+            ZavKaf zavKaf = new ZavKaf();
+
+            teacher.Lesson();
+            student.Name();
+            student.Age();
+        }
+    }
+
+}
